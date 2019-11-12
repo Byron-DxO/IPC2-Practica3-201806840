b4e1cc51a3dd3418d7c9a8e499914ee9842eddda
Byron-DxO <57022159+Byron-DxO@users.noreply.github.com>
2019-11-11 18:13:01 -0600

    Comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'page', pathMatch: 'full' },
+{ path: 'page', loadChildren: './page/page.module#PagePageModule' },
+  { path: 'registro', loadChildren: './pages/registro/registro.module#RegistroPageModule' },
+  { path: 'inicio', loadChildren: './pages/inicio/inicio.module#InicioPageModule' },
 ];
 
 @NgModule({
